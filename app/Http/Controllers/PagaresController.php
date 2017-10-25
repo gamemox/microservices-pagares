b5b16a8720b23b0c9b64fc0fad99d2f5ec16e8fa
gamemox <32118594+gamemox@users.noreply.github.com>
2017-10-25 00:56:42 -0300

    implementacion return

--- app/Http/Controllers/PagaresController.php
+++ app/Http/Controllers/PagaresController.php
@@ -7,32 +7,36 @@ use App\Pagares;
 
 class PagaresController extends Controller
 {
-  function getPagaresValidos(Request $request,$rut){
+  
+   
+   function getPagaresValidos(Request $request,$rut){
+        header("Access-Control-Allow-Origin: *");
+        header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+        header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+        header('Access-Control-Allow-Credentials: true');  
         $db = app('db');
-        $results = $db->select("SELECT p.*,t.* FROM pagaresfc p,  tipocred t
+        $results = $db->select("SELECT p.* 
+                                FROM pagaresfc p
                                 where p.knumerut=".$rut. "
+                                    and exists (select 'a' from tipocred t where t.ptipcred=p.ptipcred and sifcuweb_activo='S')
                                     and esvalido=1
-                                    and p.ptipcred=t.ptipcred 
-                                    and sifcuweb_activo='S'");
-        
-        return response()->json([
-            "contador" => count($results),
-            "datos" => $results,
-        ]);
+                                    order by anhoproc asc, semestre asc");
+        return  json_encode($results);
        
    }
    
    function getPagaresAll(Request $request,$rut){
+        header("Access-Control-Allow-Origin: *");
+        header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+        header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+        header('Access-Control-Allow-Credentials: true');  
         $db = app('db');
-        $results = $db->select("SELECT p.*,t.* FROM pagaresfc p,  tipocred t
+        $results = $db->select("SELECT p.* 
+                                FROM pagaresfc p
                                 where p.knumerut=".$rut. "
-                                    and p.ptipcred=t.ptipcred 
-                                    and sifcuweb_activo='S'");
-        
-        return response()->json([
-            "contador" => count($results),
-            "datos" => $results,
-        ]);
+                                    and exists (select 'a' from tipocred t where t.ptipcred=p.ptipcred and sifcuweb_activo='S')
+                                    order by anhoproc asc, semestre asc ");
+        return  json_encode($results);
        
    }
 }
